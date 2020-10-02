bf5c793c17d573946ab0ebafae4a4ad1a14328ab
Atiq0629 <cross.016x@gmail.com>
2020-10-03 00:12:58 +0600

    First Commit

---
+++ assets/scripts/vendor.js
@@ -0,0 +1,56 @@
+const monsterHealthBar = document.getElementById('monster-health');
+const playerHealthBar = document.getElementById('player-health');
+const bonusLifeEl = document.getElementById('bonus-life');
+
+const attackBtn = document.getElementById('attack-btn');
+const strongAttackBtn = document.getElementById('strong-attack-btn');
+const healBtn = document.getElementById('heal-btn');
+const logBtn = document.getElementById('log-btn');
+
+// const monsterDamage = document.getElementById('mon_damage')
+// const monsterCurrentHealth = document.getElementById('mon_cur_health');
+// const playerDamage = document.getElementById('play_damage');
+// const playerCurrentHealth = document.getElementById('play_cur_health');
+
+function adjustHealthBars(maxLife) {
+  monsterHealthBar.max = maxLife;
+  monsterHealthBar.value = maxLife;
+  playerHealthBar.max = maxLife;
+  playerHealthBar.value = maxLife;
+}
+
+function dealMonsterDamage(damage) {
+  const dealtDamage = Math.floor(Math.random() * damage);
+  monsterHealthBar.value = +monsterHealthBar.value - dealtDamage;
+  return dealtDamage;
+}
+
+function dealPlayerDamage(damage) {
+  const dealtDamage = Math.floor(Math.random() * damage);
+  playerHealthBar.value = +playerHealthBar.value - dealtDamage;
+  return dealtDamage;
+}
+
+function increasePlayerHealth(healValue) {
+  playerHealthBar.value = +playerHealthBar.value + healValue;
+}
+
+function resetGame(value) {
+  playerHealthBar.value = value;
+  monsterHealthBar.value = value;
+}
+
+function removeBonusLife() {
+  bonusLifeEl.parentNode.removeChild(bonusLifeEl);
+}
+
+function setPlayerHealth(health) {
+  playerHealthBar.value = health;
+}
+
+// function monDamsStatusReport(result){
+//   monsterDamage.value = result;
+// }
+// function monCurStatus(result){
+//   monsterCurrentHealth.value = result;
+// }
